1177faba8e4ced93b902f38379aa15e0624c9ff3
David Morrin <dm187@nyu.edu>
2020-05-08 15:01:01 -0400

    feat: upgrade fetch cli util with commander
    adds command line args and POST testing
    TODO: still needs authentication options

--- test/fetch.js
+++ test/fetch.js
@@ -1,36 +1,59 @@
 /**
  * a tool for quickly pinging the development app's API
  * usage: node path/to/thisFile.js apiPath
- * example: node fetch.js # if PORT=3000, tries localhost:3000/
- * example: node fetch.js /api/data # tries localhost:3000/api/data
- * !WIP
- * TODO add POST
- * TODO add support for auth (cookies, tokens)
+ *
+ * example: node fetch.js
+ * example: node fetch.js /api/data
+ *
+ * uses the project's .env file for auto-configuration
+ * TODO add authentication options (cookies, jwt, etc)
  */
+const { program } = require("commander");
 const dotenv = require("dotenv");
 const dotenvExpand = require("dotenv-expand");
 const http = require("http");
 
 dotenvExpand(dotenv.config({ path: ".env" }));
 
-const args = process.argv.slice(2);
-const path = args[0] || "";
+program
+  .version("0.0.1")
+  .arguments("[path]")
+  .action(function (path) {
+    this.path = path;
+  })
+  .option("-h|--host <url>", "host", "localhost")
+  .option("-d|--data <body>", "data (implicit POST)");
+
+program.parse(process.argv);
+
+console.log(program);
+
 const options = {
-  host: "localhost",
+  host: program.host,
   port: process.env.PORT,
-  path,
-  method: "GET",
+  path: program.path || "/",
+  method: program.data ? "POST" : "GET",
 };
 
+if (program.data) {
+  options.headers = {
+    "Content-Type": "application/json",
+    "Content-Length": program.data.length,
+  };
+}
+
 const onResponse = (res) => {
-  console.log(`STATUS: ${res.statusCode}
-  HEADERS: ${JSON.stringify(res.headers)}
-  `);
+  console.log(
+    `STATUS: ${res.statusCode}\n\n`,
+    `HEADERS: ${JSON.stringify(res.headers, null, 2)}\n\n`
+  );
   res.setEncoding("utf8");
   res.on("data", (chunk) => console.log(`BODY: ${chunk}`));
 };
 const req = http.request(options, onResponse);
 
 req.on("error", console.error);
+
+program.data && req.write(program.data);
 
 req.end();
