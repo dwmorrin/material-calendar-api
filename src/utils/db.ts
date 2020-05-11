bb936a622984c507ec1786b15c7090f3068e1bf4
David Morrin <dm187@nyu.edu>
2020-05-11 13:50:17 -0400

    feat: db connection utility

---
+++ src/utils/db.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { User, defaultUser } from "../models/user/user.model";
+
+const databaseInit = (): void => {
+  // check for any minimal data needed to be present and create if needed
+  (async (): Promise<void> => {
+    const numberOfUsers = await User.find().countDocuments().exec();
+    if (numberOfUsers === 0) {
+      const user = await User.create(defaultUser);
+      console.log("No users found.  Created one.", { user });
+    }
+  })();
+};
+
+const connect = (url = process.env.DATABASE_URL): void => {
+  if (!url) {
+    console.error("No path to database set, aborting");
+    process.exit(1);
+  }
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .then(databaseInit)
+    .catch(console.error);
+};
+
+export default connect;
