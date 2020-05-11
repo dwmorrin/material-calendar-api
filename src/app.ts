78010cb54069123f7572f9ea2e0c079f02ec538c
David Morrin <dm187@nyu.edu>
2020-05-11 13:51:01 -0400

    feat: adds user model router

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,20 @@
 import dotenv from "dotenv";
 import dotenvExpand from "dotenv-expand";
 import express from "express";
-import mongoose from "mongoose";
+import userRouter from "./models/user/user.router";
+import dbConnect from "./utils/db";
 
 dotenvExpand(dotenv.config({ path: ".env" }));
 
 // MongoDB connection
-const mongoUrl = process.env.DATABASE_URL;
-if (!mongoUrl) {
-  console.error("No path to database set, aborting");
-  process.exit(1);
-}
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then()
-  .catch(console.error);
+dbConnect();
 
 // configure express
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 5000);
 
-// middleware
 app.use(express.json());
 
-app.get("/*", (req, res) => {
-  console.log(`${req.method} ${req.path}`);
-  return res.status(200).json({ message: "OK" });
-});
-
-app.post("/*", (req, res) => {
-  console.log(`${req.method} ${req.path} ${JSON.stringify(req.body, null, 2)}`);
-  return res.status(201).json({ message: "OK" });
-});
+app.use("/api/user", userRouter);
 
 export default app;
