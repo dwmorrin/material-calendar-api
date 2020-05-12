55807ee4c628d301f2a98538371cf7f668b21b72
David Morrin <dm187@nyu.edu>
2020-05-12 10:19:48 -0400

    fix: change controllers to promises

--- src/models/event/event.controller.ts
+++ src/models/event/event.controller.ts
@@ -1,87 +1,52 @@
 import { Event } from "./event.model";
 import { Request, Response } from "express";
 
-export const createOne = () => async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const event = await Event.create(req.body);
-    res.status(201).json({ data: event });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const createOne = (req: Request, res: Response) => {
+  Event.create(req.body)
+    .then((data) => res.status(201).json({ data }))
+    .catch((error) => {
+      console.error(error);
+      res.status(400).end();
+    });
 };
 
-export const getMany = () => async (
-  _: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const events = await Event.find().lean().exec();
-    res.status(200).json({ data: events });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const getMany = (_: Request, res: Response) => {
+  Event.find()
+    .lean()
+    .then((data) => res.status(200).json({ data }))
+    .catch((error) => {
+      console.error(error);
+      res.status(400).end();
+    });
 };
 
-export const getOne = () => async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const event = await Event.findOne({ _id: req.params.id }).lean().exec();
-
-    if (!event) {
-      return res.status(400).end();
-    }
-    res.status(200).json({ data: event });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const getOne = (req: Request, res: Response) => {
+  Event.findOne({ _id: req.params.id })
+    .lean()
+    .then((data) => res.status(200).json({ data }))
+    .catch((error) => {
+      console.error(error);
+      res.status(400).end();
+    });
 };
 
-export const removeOne = () => async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const event = await Event.findOneAndRemove({ _id: req.params.id });
-
-    if (!event) {
-      return res.status(400).end();
-    }
-    res.status(200).json({ data: event });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const removeOne = (req: Request, res: Response) => {
+  Event.findOneAndRemove({ _id: req.params.id })
+    .then((data) => res.status(200).json({ data }))
+    .catch((error) => {
+      console.error(error);
+      res.status(400).end();
+    });
 };
 
-export const updateOne = () => async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const event = await Event.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body,
-      {
-        new: true,
-      }
-    )
-      .lean()
-      .exec();
-
-    if (!event) {
-      return res.status(400).end();
-    }
-    res.status(200).json({ data: event });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const updateOne = (req: Request, res: Response) => {
+  Event.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+  })
+    .lean()
+    .then((data) => res.status(200).json({ data }))
+    .catch((error) => {
+      console.log(error);
+      res.status(400).end();
+    });
 };
--- src/models/project/project.controller.ts
+++ src/models/project/project.controller.ts
@@ -1,87 +1,52 @@
 import { Project } from "./project.model";
 import { Request, Response } from "express";
 
-export const createOne = () => async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const project = await Project.create(req.body);
-    res.status(201).json({ data: project });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const createOne = (req: Request, res: Response) => {
+  Project.create(req.body)
+    .then((data) => res.status(201).json({ data }))
+    .catch((error) => {
+      console.error(error);
+      res.status(400).end();
+    });
 };
 
-export const getMany = () => async (
-  _: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const projects = await Project.find().lean().exec();
-    res.status(200).json({ data: projects });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const getMany = (_: Request, res: Response) => {
+  Project.find()
+    .lean()
+    .then((data) => res.status(200).json({ data }))
+    .catch((error) => {
+      console.error(error);
+      res.status(400).end();
+    });
 };
 
-export const getOne = () => async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const project = await Project.findOne({ _id: req.params.id }).lean().exec();
-
-    if (!project) {
-      return res.status(400).end();
-    }
-    res.status(200).json({ data: project });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const getOne = (req: Request, res: Response) => {
+  Project.findOne({ _id: req.params.id })
+    .lean()
+    .then((data) => res.status(200).json({ data }))
+    .catch((error) => {
+      console.error(error);
+      res.status(400).end();
+    });
 };
 
-export const removeOne = () => async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const project = await Project.findOneAndRemove({ _id: req.params.id });
-
-    if (!project) {
-      return res.status(400).end();
-    }
-    res.status(200).json({ data: project });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const removeOne = (req: Request, res: Response) => {
+  Project.findOneAndRemove({ _id: req.params.id })
+    .then((data) => res.status(200).json({ data }))
+    .catch((error) => {
+      console.error(error);
+      res.status(400).end();
+    });
 };
 
-export const updateOne = () => async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
-    const project = await Project.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body,
-      {
-        new: true,
-      }
-    )
-      .lean()
-      .exec();
-
-    if (!project) {
-      return res.status(400).end();
-    }
-    res.status(200).json({ data: project });
-  } catch (error) {
-    console.error(error);
-    res.status(400).end();
-  }
+export const updateOne = (req: Request, res: Response) => {
+  Project.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+  })
+    .lean()
+    .then((data) => res.status(200).json({ data }))
+    .catch((error) => {
+      console.log(error);
+      res.status(400).end();
+    });
 };
