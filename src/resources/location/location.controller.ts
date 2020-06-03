2f74918ddad8d1dc3c2085fee65ca57dc28ab0fe
David Morrin <dm187@nyu.edu>
2020-06-03 13:49:03 -0400

    fix: add location SQL

--- src/resources/location/location.controller.ts
+++ src/resources/location/location.controller.ts
@@ -1,10 +1,26 @@
 import { Request, Response } from "express";
 import { controllers } from "../../utils/crud";
+import pool, { error500 } from "../../utils/db";
 
-export const getMany = (req: Request, res: Response) => {
-  res
-    .status(500)
-    .json({ error: { code: 500, message: "TODO get many location" } });
-};
+const query = `
+  SELECT
+    id,
+    name as title,
+    location as groupId
+  FROM
+    studio
+`;
 
-export default controllers("studio", "id");
+export const getMany = (req: Request, res: Response) =>
+  pool.query(query, (err, rows) => {
+    if (err) return res.status(500).json(error500(err));
+    res.status(200).json({ data: rows, context: req.query.context });
+  });
+
+export const getOne = (req: Request, res: Response) =>
+  pool.query(query + "WHERE id = ?", [req.params.id], (err, rows) => {
+    if (err) return res.status(500).json(error500(err));
+    res.status(200).json({ data: rows[0], context: req.query.context });
+  });
+
+export default { ...controllers("studio", "id"), getMany, getOne };
