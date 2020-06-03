c73e5015ee6f2ad9e1daabda7c816a242cfe80c1
David Morrin <dm187@nyu.edu>
2020-06-03 15:13:29 -0400

    fix: add project & group info

--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -40,30 +40,46 @@ export const getOneGroup = (req: Request, res: Response) => {
   );
 };
 
-const userQuery = `
+const userQueryFn = (where = "") => `
   SELECT 
-    user_id AS username,
-    JSON_OBJECT('first', first_name, 'last', last_name) AS name,
-    JSON_ARRAY(CASE user_type
-        WHEN 1 THEN 'admin'
-        WHEN 2 THEN 'admin'
-        WHEN 3 THEN 'user'
-        WHEN 4 THEN 'staff'
-      END) AS roles,
-    JSON_OBJECT('email', JSON_ARRAY(email)) AS contact
+      u.user_id AS username,
+      JSON_OBJECT('first',
+              u.first_name,
+              'last',
+              u.last_name) AS name,
+      JSON_ARRAY(CASE u.user_type
+                  WHEN 1 THEN 'admin'
+                  WHEN 2 THEN 'admin'
+                  WHEN 3 THEN 'user'
+                  WHEN 4 THEN 'staff'
+              END) AS roles,
+      JSON_OBJECT('email', JSON_ARRAY(u.email)) AS contact,
+      IF(rg.project_id IS NOT NULL,
+          JSON_ARRAYAGG(JSON_OBJECT('id', rg.project_id, 'groupId', rg.id)),
+          JSON_ARRAY()) AS projects
   FROM
-    user
+      user u
+          LEFT JOIN
+      student_group sg ON sg.student_id = u.id
+          LEFT JOIN
+      rm_group rg ON rg.id = sg.group_id
+  ${where}
+  GROUP BY u.id
 `;
 
 export const getOne = (req: Request, res: Response) => {
-  pool.query(userQuery + "WHERE user_id = ?", [req.params.id], (err, rows) => {
-    if (err) return res.status(500).json(error500(err));
-    res.status(200).json({ data: rows[0], context: req.query.context });
-  });
+  pool.query(
+    userQueryFn("WHERE u.user_id = ?"),
+    [req.params.id],
+    (err, rows) => {
+      if (err) return res.status(500).json(error500(err));
+      res.status(200).json({ data: rows[0], context: req.query.context });
+    }
+  );
 };
 
 export const getMany = (req: Request, res: Response) => {
-  pool.query(userQuery, (err, rows) => {
+  pool.query(userQueryFn(), (err, rows) => {
     if (err) return res.status(500).json(error500(err));
     res.status(200).json({ data: rows, context: req.query.context });
   });
