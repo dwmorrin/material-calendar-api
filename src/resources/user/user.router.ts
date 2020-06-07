33079cb58568eb6eed1b84aeabfe714c1b6ea288
David Morrin <dm187@nyu.edu>
2020-06-07 14:10:34 -0400

    feat: add routes for resources scoped to user

--- src/resources/user/user.router.ts
+++ src/resources/user/user.router.ts
@@ -3,9 +3,16 @@ import controller from "./user.controller";
 
 const router = Router();
 
+// User Groups
 router.get("/groups", controller.getGroups);
 router.get("/groups/:id", controller.getOneGroup);
 
+// Get collections by username
+router.get("/:id/courses", controller.getCourses);
+router.get("/:id/groups", controller.getGroupsForOne);
+router.get("/:id/projects", controller.getProjects);
+
+// user records
 router.delete("/:id", controller.removeOne);
 router.get("/", controller.getMany);
 router.get("/:id", controller.getOne);
