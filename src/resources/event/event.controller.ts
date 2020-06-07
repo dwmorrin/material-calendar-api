492bde56e78c787dc87349bbc94376bdc201ea05
David Morrin <dm187@nyu.edu>
2020-06-07 14:09:51 -0400

    fix: add reservation details to event info

--- src/resources/event/event.controller.ts
+++ src/resources/event/event.controller.ts
@@ -1,35 +1,49 @@
 import { controllers } from "../../utils/crud";
 import { Request, Response } from "express";
-import pool, { error500, mapKeysToBool } from "../../utils/db";
+import pool, { error500, mapKeysToBool, inflate } from "../../utils/db";
+import { compose } from "ramda";
 
 const query = `
   SELECT 
     id,
     start,
     end,
-    studio AS location,
+    JSON_OBJECT(
+      'id', studioId,
+      'title', studio
+    ) AS location,
     description AS title,
-    open as reservable
+    open AS reservable,
+    IF (
+      reservationId IS NOT NULL,
+      JSON_OBJECT(
+        'id', reservationId,
+        'groupId', projectGroupId,
+        'equipment', gear 
+      ),
+      NULL
+    ) AS reservation
   FROM
     full_calendar
 `;
 
+const process = compose(mapKeysToBool("reservable"), inflate);
+
 const getMany = (req: Request, res: Response) =>
   pool.query(query, (err, rows) => {
     if (err) return res.status(500).json(error500(err));
-    const data = rows.map((event: {}) => mapKeysToBool(event, "reservable"));
-    res.status(200).json({ data, context: req.query.context });
+    res
+      .status(200)
+      .json({ data: rows.map(process), context: req.query.context });
   });
 
 const getOne = (req: Request, res: Response) =>
   pool.query(query + "WHERE id = ?", [req.params.id], (err, rows) => {
     if (err) return res.status(500).json(error500(err));
-    res
-      .status(200)
-      .json({
-        data: mapKeysToBool(rows[0], "reservable"),
-        context: req.query.context,
-      });
+    res.status(200).json({
+      data: rows.map(process)[0],
+      context: req.query.context,
+    });
   });
 
 export default { ...controllers("allotment", "id"), getMany, getOne };
