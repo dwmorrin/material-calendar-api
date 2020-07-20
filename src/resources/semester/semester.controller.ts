d4efd1121ef8dce111e479c4e422eb0ad56b74c3
David Morrin <dm187@nyu.edu>
2020-07-20 13:26:03 -0400

    fix: semester: adds adapter to PUT requests

--- src/resources/semester/semester.controller.ts
+++ src/resources/semester/semester.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import pool, { mapKeysToBool } from "../../utils/db";
 import { controllers, onResult } from "../../utils/crud";
+import { omit } from "ramda";
 
 const makeActiveBoolean = mapKeysToBool("active");
 
@@ -24,23 +25,34 @@ export const getCurrent = (req: Request, res: Response) =>
 export const getMany = (req: Request, res: Response) =>
   pool.query(query, onResult({ req, res, dataMapFn: makeActiveBoolean }).read);
 
-const adapter = (semester: { id: number; title: string }): {} => {
-  const copy = { ...semester };
-  delete copy.id;
-  delete copy.title;
-  return copy;
-};
+/**
+ * temporary adapter to matchup new column names to old
+ */
+const adapter = (semester: { title: string }): { name: string } => ({
+  ...semester,
+  name: semester.title,
+});
 
 export const createOne = (req: Request, res: Response) =>
   pool.query(
     "INSERT INTO semester SET ?",
-    [adapter(req.body)],
+    [omit(["id", "title"], adapter(req.body))],
     onResult({ req, res }).create
   );
+
+export const updateOne = (req: Request, res: Response) => {
+  console.log(req.body);
+  pool.query(
+    "UPDATE semester SET ? WHERE id = ?",
+    [omit(["title"], adapter(req.body)), req.params.id],
+    onResult({ req, res }).update
+  );
+};
 
 export default {
   ...controllers("semester", "id"),
   createOne,
   getCurrent,
   getMany,
+  updateOne,
 };
