a8d4986d705e7551fbf55bd921506a5903d2b11d
David Morrin <dm187@nyu.edu>
2021-06-17 11:32:13 -0400

    feat: adds custom createOne for projects

--- src/resources/project/project.controller.ts
+++ src/resources/project/project.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import pool, { error500, inflate } from "../../utils/db";
 import { controllers, onResult } from "../../utils/crud";
+import { MysqlError } from "mysql";
 
 const groupQuery = (where = "") => `
 SELECT
@@ -85,8 +86,91 @@ export const getMany = (req: Request, res: Response): void => {
   );
 };
 
+export const createOne = (req: Request, res: Response): void => {
+  // massage data into a suitable form
+  const {
+    course,
+    end,
+    groupAllottedHours,
+    groupSize,
+    locationIds,
+    open,
+    reservationStart,
+    start,
+    title,
+  } = req.body;
+  const project = {
+    book_start: reservationStart,
+    end,
+    group_hours: groupAllottedHours,
+    group_size: groupSize,
+    open,
+    start,
+    title,
+  };
+
+  const locationIdsMap =
+    (projectId: string | number) => (locationId: string | number) =>
+      [projectId, locationId];
+
+  pool.query(`INSERT INTO project SET ?`, [project], (error, results) => {
+    if (error) return onError(error);
+    const projectId = results.insertId;
+    const projectLocations = locationIds.map(locationIdsMap(projectId));
+    if (Number(course?.id) > 0) {
+      createCourseProjectAndProjectLocations(projectId, projectLocations);
+    } else {
+      if (locationIds.length)
+        createProjectLocations(projectId, projectLocations);
+      else onSuccess(projectId);
+    }
+  });
+
+  function createCourseProjectAndProjectLocations(
+    projectId: string | number,
+    projectLocations: { [k: string]: string | number }[]
+  ) {
+    pool.query(
+      `INSERT INTO course_project SET ?`,
+      [{ course_id: course.id, project_id: projectId }],
+      (error) => {
+        if (error) return onError(error);
+        if (locationIds.length)
+          createProjectLocations(projectId, projectLocations);
+        else onSuccess(projectId);
+      }
+    );
+  }
+
+  function onError(error: MysqlError) {
+    res.status(500).json(error500(error, req.query.context));
+  }
+
+  function createProjectLocations(
+    projectId: string | number,
+    projectLocations: { [k: string]: string | number }[]
+  ) {
+    pool.query(
+      `REPLACE INTO project_studio (project_id, studio_id) VALUES ?`,
+      [projectLocations],
+      (error) => {
+        if (error) return onError(error);
+        onSuccess(projectId);
+      }
+    );
+  }
+
+  function onSuccess(projectId: string | number) {
+    res.status(201).json({
+      data: { ...req.body, id: projectId },
+      context: req.query.context,
+    });
+  }
+};
+
 export default {
   ...controllers("project", "id"),
+  createOne,
   getMany,
   getOneLocationAllotment,
   getGroupsByProject,
