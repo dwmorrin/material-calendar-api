c8310bb27ff6aae20ed485eae6212c97a1ce373c
David Morrin <dm187@nyu.edu>
2021-06-26 20:29:34 -0400

    fix: aborts if zero records

--- sql/read-data.mjs
+++ sql/read-data.mjs
@@ -4,8 +4,23 @@
 import { readFile } from "fs";
 import { createPool } from "mysql";
 
+/**
+ * A table name mapped to an array of table records.
+ * @typedef {Record<string,unknown[]>} Tables
+ */
+/**
+ * Structure of the JSON data file
+ * @typedef {Object} Schema
+ * @property {string} host
+ * @property {number} port
+ * @property {string} user
+ * @property {string} password
+ * @property {string} database
+ * @property {Tables} tables
+ */
+
 const filename = process.argv[2];
-if (typeof filename !== "string") {
+if (!filename) {
   console.error("which data file?");
   process.exit(1);
 }
@@ -15,7 +30,16 @@ readFile(filename, "utf8", (error, data) => {
   insertRecords(JSON.parse(data));
 });
 
+/**
+ * @param {Schema} schema
+ */
 function insertRecords(schema) {
+  const { tables } = schema;
+  const totalRecords = countRecords(tables);
+  if (!totalRecords) {
+    console.error("no records found. nothing to do.");
+    process.exit(1);
+  }
   const pool = createPool({
     host: schema.host,
     user: schema.user,
@@ -23,7 +47,7 @@ function insertRecords(schema) {
     database: schema.database,
   });
   // end pool after all records have been inserted
-  const onInsert = withCountInserts(schema, () => pool.end());
+  const onInsert = withCountTo(totalRecords, () => pool.end());
   Object.entries(schema.tables).forEach(([table, records]) => {
     records.forEach((record) => {
       pool.query(`INSERT INTO ?? SET ?`, [table, record], onInsert);
@@ -32,21 +56,29 @@ function insertRecords(schema) {
 }
 
 /**
- * Queries run in parallel, so if we know how many records there are,
- * we can execute a callback after all records have been read.
+ * Returns a function that counts each time the return function is called.
+ * When the return function is called `count` times, the provided callback
+ * function is executed.
+ * @param {number} count execute callback
+ * @param {() => void} cb callback function
+ * @returns {(error: MysqlError) => void}
  */
-function withCountInserts(schema, cb) {
-  let inserts = 0;
-  const totalInserts = countRecords(schema);
+function withCountTo(count, cb) {
+  if (count < 1) throw new Error("count must be greater than zero");
+  let fnCallCount = 0;
   return (error) => {
-    ++inserts;
+    ++fnCallCount;
     if (error) console.error(error);
-    if (inserts === totalInserts) cb();
+    if (fnCallCount === count) cb();
   };
 }
 
-function countRecords(schema) {
-  return Object.values(schema.tables).reduce(
+/**
+ * @param {Tables} tables
+ * @returns {number}
+ */
+function countRecords(tables) {
+  return Object.values(tables).reduce(
     (total, records) => total + records.length,
     0
   );
