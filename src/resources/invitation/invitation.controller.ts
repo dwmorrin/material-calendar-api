4dd14d7a449ade66b6771318023ddb6054133ad7
Michael Hagen <michaelhagen@nyu.edu>
2021-07-08 11:03:08 -0400

    FIX: Fixes bug which returned invitation invitees as 2^n duplicates

--- src/resources/invitation/invitation.controller.ts
+++ src/resources/invitation/invitation.controller.ts
@@ -18,20 +18,24 @@ export const getInvitations = (req: Request, res: Response): Query =>
             uin.first_name,
             'last',
             uin.last_name)) as invitor,
-            JSON_ARRAYAGG(JSON_OBJECT('id',
-           vt.invitee,
-           'name',
-           JSON_OBJECT('first',
-            uiv.first_name,
-            'last',
-            uiv.last_name),
-            'accepted',
-            vt.accepted,'rejected',vt.rejected)) AS invitees,
+            tv.invitee AS invitees,
+            tv.invitation_id AS insssvitees,
             (SELECT (CASE WHEN COUNT(iv.accepted)=SUM(iv.accepted) THEN 1 ELSE 0 END)) as confirmed,
             rm.id as group_id
             from invitation inv left join invitee iv on inv.id=iv.invitation_id 
-            left join invitee vt on vt.invitation_id=inv.id
-            left join user uiv on uiv.id=vt.invitee
+            left join 
+              (select vt.invitation_id as invitation_id, json_arrayagg(JSON_Object('id',
+              vt.invitee,
+              'name',
+              JSON_OBJECT('first',
+              uiv.first_name,
+              'last',
+              uiv.last_name),
+              'accepted',
+              vt.accepted,'rejected',vt.rejected)) AS invitee 
+              from invitee vt 
+              left join user uiv on uiv.id=vt.invitee 
+              group by vt.invitation_id) tv on tv.invitation_id=inv.id
             left join user uin on uin.id=inv.invitor
             left join rm_group rm on uin.id=rm.creator and inv.project_id=rm.project_id
             where (iv.invitee=? or inv.invitor=?) group by inv.id;`,
@@ -50,20 +54,24 @@ export const getInvitationsByProject = (req: Request, res: Response): Query =>
             uin.first_name,
             'last',
             uin.last_name)) as invitor,
-            JSON_ARRAYAGG(JSON_OBJECT('id',
-           vt.invitee,
-           'name',
-           JSON_OBJECT('first',
-            uiv.first_name,
-            'last',
-            uiv.last_name),
-            'accepted',
-            vt.accepted,'rejected',vt.rejected)) AS invitees,
+            tv.invitee AS invitees,
+            tv.invitation_id AS insssvitees,
             (SELECT (CASE WHEN COUNT(iv.accepted)=SUM(iv.accepted) THEN 1 ELSE 0 END)) as confirmed,
             rm.id as group_id
             from invitation inv left join invitee iv on inv.id=iv.invitation_id 
-            left join invitee vt on vt.invitation_id=inv.id
-            left join user uiv on uiv.id=vt.invitee
+            left join 
+              (select vt.invitation_id as invitation_id, json_arrayagg(JSON_Object('id',
+              vt.invitee,
+              'name',
+              JSON_OBJECT('first',
+              uiv.first_name,
+              'last',
+              uiv.last_name),
+              'accepted',
+              vt.accepted,'rejected',vt.rejected)) AS invitee 
+              from invitee vt 
+              left join user uiv on uiv.id=vt.invitee 
+              group by vt.invitation_id) tv on tv.invitation_id=inv.id
             left join user uin on uin.id=inv.invitor
             left join rm_group rm on uin.id=rm.creator and inv.project_id=rm.project_id
             where inv.project_id=? and (iv.invitee=? or inv.invitor=?) group by inv.id;`,
