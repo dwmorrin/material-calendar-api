b6e8db36f8652acae7417488900fc44ad449cd74
Michael Hagen <michaelhagen@nyu.edu>
2021-07-19 13:21:26 -0400

    Fix: Missing Parameter in invitation acceptance endpoint

--- src/resources/group/group.router.ts
+++ src/resources/group/group.router.ts
@@ -9,7 +9,7 @@ router.get("/:groupId", controller.getOneGroup);
 router.get("/user/:userId", controller.getGroupsByUser);
 router.get("/project/:projectId", controller.getGroupsByProject);
 router.delete("/:groupId", controller.removeOneGroup);
-router.post("/invitation/invitationId", controller.createGroupFromInvitation);
+router.post("/invitation/:invitationId", controller.createGroupFromInvitation);
 
 // Joining and Leaving Groups
 router.post("/:groupId/invitation/:invitationId", controller.joinGroup);
