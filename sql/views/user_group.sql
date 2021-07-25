d386cacc266606f04fb627900a776095ba248f45
Michael Hagen <michaelhagen@nyu.edu>
2021-07-25 11:27:01 -0400

    Adjust user_group view to cast member.id as string since json_search only works on strings

--- sql/views/user_group.sql
+++ sql/views/user_group.sql
@@ -11,7 +11,7 @@ FROM
       rg.project_id AS projectId,
       JSON_ARRAYAGG(
         JSON_OBJECT(
-          'id', u.id,
+          'id', CAST(u.id AS CHAR CHARSET UTF8MB4),
           'username', u.user_id,
           'name', JSON_OBJECT('first', u.first_name,'last',u.last_name),
           'email', u.email
