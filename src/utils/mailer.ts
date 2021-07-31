3a2d0d2b13fb6648c44c67c8505bc1b6040a0336
David Morrin <dm187@nyu.edu>
2021-07-31 11:54:07 -0400

    feat: factors out mailer & adds useMailbox utility

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,9 +1,30 @@
 import nodemailer from "nodemailer";
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+import { EC } from "./types";
+
+const mailer = nodemailer.createTransport({
+  host: "localhost",
+  port: Number(process.env.EMAIL_PORT) || 25,
+  secure: false,
+  tls: { rejectUnauthorized: false },
+});
+
+export const useMailbox: EC = (req, res, next) => {
+  const { mailbox } = res.locals;
+  if (!Array.isArray(mailbox))
+    return next(new Error("useMail called without mailbox on res.locals"));
+  if (!mailbox.length) return next();
+  const mail = mailbox.pop();
+  mailer.sendMail(mail, (err) => {
+    if (err) return next(err);
+    // recursively call useMailbox until mailbox is empty
+    useMailbox(req, res, next);
+  });
+};
 
 //-------- mailer.controller ---- //
 
-function send(req: Request, res: Response) {
+const send: EC = (req, res) => {
   const { to = "", subject = "", text = "", html = "" } = req.body;
   const mail = {
     from: process.env.EMAIL_FROM || '"Booking App" <admin@booking.app>',
@@ -11,18 +32,11 @@ function send(req: Request, res: Response) {
     subject,
     text,
   };
-  nodemailer
-    .createTransport({
-      host: "localhost",
-      port: Number(process.env.EMAIL_PORT) || 25,
-      secure: false,
-      tls: { rejectUnauthorized: false },
-    })
-    .sendMail(html ? { ...mail, html } : mail, (error, info) => {
-      if (error) res.status(500).json({ error });
-      else res.status(201).json({ data: info });
-    });
-}
+  mailer.sendMail(html ? { ...mail, html } : mail, (error, info) => {
+    if (error) res.status(500).json({ error });
+    else res.status(201).json({ data: info });
+  });
+};
 
 //----- mailer.router -----//
 const router = Router();
