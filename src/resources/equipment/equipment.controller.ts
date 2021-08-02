007faa559658004aac34e7b27e621287c13c4860
David Morrin <dm187@nyu.edu>
2021-08-02 10:32:14 -0400

    fix: adds update one equipment

--- src/resources/equipment/equipment.controller.ts
+++ src/resources/equipment/equipment.controller.ts
@@ -2,16 +2,54 @@ import { controllers, addResultsToResponse } from "../../utils/crud";
 import pool from "../../utils/db";
 import { EC } from "../../utils/types";
 
-const query = `
-  SELECT
-    *
-  FROM equipment_view
-  WHERE
-    JSON_SEARCH(category, 'one', ?)
-  GROUP BY id
-`;
+const getByCategory: EC = (req, res, next) =>
+  pool.query(
+    `SELECT * FROM equipment_view
+    WHERE JSON_SEARCH(category, 'one', ?)
+    GROUP BY id`,
+    [req.params.id],
+    addResultsToResponse(res, next)
+  );
 
-export const getByCategory: EC = (req, res, next) =>
-  pool.query(query, [req.params.id], addResultsToResponse(res, next));
+// TODO implement tags
+const updateOne: EC = (req, res, next) => {
+  const {
+    category: { id },
+    manufacturer,
+    model,
+    description,
+    sku,
+    serial,
+    barcode,
+    quantity,
+    consumable,
+    notes,
+    restriction,
+  } = req.body;
+  pool.query(
+    "UPDATE equipment SET ? WHERE id = ?",
+    [
+      {
+        category: id,
+        manufacturer,
+        model,
+        description,
+        sku,
+        serial,
+        barcode,
+        quantity,
+        consumable,
+        notes,
+        restriction,
+      },
+      req.params.id,
+    ],
+    addResultsToResponse(res, next, { one: true })
+  );
+};
 
-export default { ...controllers("equipment_view", "id"), getByCategory };
+export default {
+  ...controllers("equipment_view", "id"),
+  getByCategory,
+  updateOne,
+};
