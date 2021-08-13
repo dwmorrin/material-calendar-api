ba4459f901798cf88d4ee0cefb88b8e50975cb9c
David Morrin <dm187@nyu.edu>
2021-08-13 12:20:05 -0400

    fix: disables invitation routes
    something is crashing typescript in the invite routes
    
    the db restructure may eliminate this route anyways

--- src/resources/invitation/invitation.router.ts
+++ src/resources/invitation/invitation.router.ts
@@ -4,18 +4,18 @@ import { sendResults } from "../../utils/crud";
 
 const router = Router();
 
-router.get("/user/:userId", controller.getInvitations);
-router.get(
-  "/user/:userId/project/:projectId",
-  controller.getInvitationsByProject
-);
-router.get("/exceptions", controller.getInvitationsPendingAdminApproval);
-router.put("/exceptions/:invitationId", controller.adminResponse);
+// router.get("/user/:userId", controller.getInvitations);
+// router.get(
+//   "/user/:userId/project/:projectId",
+//   controller.getInvitationsByProject
+// );
+// router.get("/exceptions", controller.getInvitationsPendingAdminApproval);
+// router.put("/exceptions/:invitationId", controller.adminResponse);
 
-router.post("/", controller.createInvitations);
-router.put("/:invitationId", controller.updateInvitation);
-router.delete("/:invitationId", controller.removeInvitation);
+// router.post("/", controller.createInvitations);
+// router.put("/:invitationId", controller.updateInvitation);
+// router.delete("/:invitationId", controller.removeInvitation);
 
-router.use(sendResults);
+// router.use(sendResults);
 
 export default router;
