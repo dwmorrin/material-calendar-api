0e7a4c489d6870634d9be82a34de0c5603cdc814
David Morrin <dm187@nyu.edu>
2021-08-15 13:47:54 -0400

    feat: location import uses transaction

--- src/resources/location/location.import.ts
+++ src/resources/location/location.import.ts
@@ -1,78 +1,131 @@
-import pool, { inflate } from "../../utils/db";
+/* eslint-disable no-console */
+import { Connection } from "mysql";
+import { getUnsafeMultipleStatementConnection, inflate } from "../../utils/db";
 import { withResource } from "../../utils/crud";
 import { EC, EEH } from "../../utils/types";
 import { getMany } from "./location.controller";
 
+type LocationInputRecord = {
+  title: string;
+  location: string;
+  restriction: number;
+  allowsWalkIns: boolean;
+};
+
 const setup: EC = (req, res, next) => {
-  const input = req.body;
+  const input: LocationInputRecord[] = req.body;
   if (!input || !(Array.isArray(input) && input.length))
     return next("no input");
   res.locals.input = input;
   res.locals.inserts = [];
   res.locals.updates = [];
+  res.locals.seen = {};
   next();
 };
 
-/**
- * req.body = { title: string, location: string, restriction: number }[]
- */
-const process: EC = (req, res, next) => {
-  const { input, locations = [] } = res.locals;
-  if (!res.locals.seen) res.locals.seen = {};
-  // loops until input is empty
-  if (!input || !input.length) return next();
-  const location = input.shift();
-  if (res.locals.seen[location.title]) return next("repeated title");
-  res.locals.seen[location.title] = true;
-  const existing = (
-    locations as {
-      id: number;
-      title: string;
-      location: string;
-      restriction: number;
-      allows_walk_ins: boolean;
-    }[]
-  ).find(({ title }) => title === location.title);
-  if (existing)
-    res.locals.updates.push({
-      id: existing.id,
-      title: location.title,
-      location: location.location,
-      restriction: location.restriction,
-      allows_walk_ins: location.allowsWalkIns,
-    });
-  else
-    res.locals.inserts.push({
-      title: location.title,
-      location: location.location,
-      restriction: location.restriction,
-      allows_walk_ins: location.allowsWalkIns,
-    });
-  process(req, res, next);
+interface LocationRecord {
+  id: number;
+  title: string;
+  location: string;
+  restriction: number;
+  allows_walk_ins: boolean;
+}
+
+type LocationHashTable = Record<string, true>;
+
+const process: EC = (_, res, next) => {
+  const input: LocationInputRecord[] = res.locals.input;
+  const locations: LocationRecord[] = res.locals.locations; // from withResource
+  const seen: LocationHashTable = res.locals.seen;
+  const inserts: Omit<LocationRecord, "id">[] = res.locals.inserts;
+  const updates: (Omit<LocationRecord, "id" | "title"> | number)[] =
+    res.locals.updates;
+  if (!input.length) return next();
+  input.forEach(({ title, location, restriction, allowsWalkIns }) => {
+    if (seen[title]) throw new Error("repeated title");
+    seen[title] = true;
+    const existing = locations.find(({ title: t }) => t === title);
+    if (existing) {
+      updates.push({ location, restriction, allows_walk_ins: allowsWalkIns });
+      updates.push(existing.id);
+    } else
+      inserts.push({
+        title,
+        location,
+        restriction,
+        allows_walk_ins: allowsWalkIns,
+      });
+  });
+  next();
 };
 
-const insert: EC = (req, res, next) => {
-  const { inserts } = res.locals;
-  if (!inserts || !inserts.length) return next();
-  const location = inserts.shift();
-  pool.query("INSERT INTO location SET ?", [location], (err) => {
+const startTransaction: EC = (_, res, next) => {
+  const unsafeConnection = getUnsafeMultipleStatementConnection();
+  res.locals.connection = unsafeConnection;
+  unsafeConnection.beginTransaction((err) => {
     if (err) return next(err);
-    insert(req, res, next);
+    next();
   });
 };
 
-const update: EC = (req, res, next) => {
-  const { updates } = res.locals;
-  if (!updates || !updates.length) return next();
-  const location = updates.shift();
-  pool.query(
-    "UPDATE location SET ? WHERE id = ?",
-    [location, location.id],
+const insert: EC = (_, res, next) => {
+  const connection: Connection = res.locals.connection;
+  const inserts: Omit<LocationRecord, "id">[] = res.locals.inserts;
+  if (!inserts.length) return next();
+  connection.query(
+    "INSERT INTO location SET ?;".repeat(inserts.length),
+    inserts,
     (err) => {
       if (err) return next(err);
-      update(req, res, next);
+      next();
     }
   );
+};
+
+const update: EC = (_, res, next) => {
+  const connection: Connection = res.locals.connection;
+  const updates: (Omit<LocationRecord, "id" | "title"> | number)[] =
+    res.locals.updates;
+  if (!updates.length) return next();
+  connection.query(
+    "UPDATE location SET ? WHERE id = ?;".repeat(updates.length / 2),
+    updates,
+    (err) => {
+      if (err) return next(err);
+      next();
+    }
+  );
+};
+
+const commitTransaction: EC = (_, res, next) => {
+  const connection: Connection = res.locals.connection;
+  connection.commit((err) => {
+    if (err) return next(err);
+    connection.end((error) => {
+      if (error) return next(error);
+      delete res.locals.connection;
+      next();
+    });
+  });
+};
+
+const rollbackGuard: EEH = (error, _, res, next) => {
+  const connection: Connection = res.locals.connection;
+  if (!connection) return next(error);
+  console.log("error during transaction; calling rollback");
+  connection.rollback(() => next(error));
+};
+
+const unsafeConnectionErrorHandler: EEH = (error, _, res, next) => {
+  const connection: Connection = res.locals.connection;
+  if (!connection) return next(error);
+  console.log("terminating unsafe connection due to error:");
+  console.log(error);
+  connection.end((error2) => {
+    if (error2) return next(error2);
+    delete res.locals.unsafeConnection;
+    next(error);
+  });
 };
 
 // requires getMany to be run first to populate res.locals.results
@@ -94,8 +147,12 @@ export default [
   setup,
   withResource("locations", "SELECT * FROM location"),
   process,
+  startTransaction,
   insert,
   update,
+  commitTransaction,
+  rollbackGuard,
+  unsafeConnectionErrorHandler,
   getMany,
   response,
   onError,
