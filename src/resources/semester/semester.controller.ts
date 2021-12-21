82cf9ff0a1230261cf774d6ee98786eaf22c5ccd
David Morrin <dm187@nyu.edu>
2021-12-21 13:14:32 -0500

    fix: altering semesters refreshes all
    Since an update or creation of a semester can change which is "active",
    all semesters need to be updated

--- src/resources/semester/semester.controller.ts
+++ src/resources/semester/semester.controller.ts
@@ -1,58 +1,53 @@
-import pool from "../../utils/db";
 import {
-  controllers,
-  addResultsToResponse,
   CrudAction,
+  controllers,
+  crud,
+  query,
+  respond,
 } from "../../utils/crud";
-import { EC } from "../../utils/types";
 
-const getCurrent: EC = (_, res, next) =>
-  pool.query(
-    "SELECT * FROM active_semester_view",
-    addResultsToResponse(res, next, { one: true })
-  );
+const getCurrent = crud.readOne("SELECT * FROM active_semester_view");
+const getMany = crud.readMany("SELECT * FROM semester_view");
 
-const getMany: EC = (_, res, next) =>
-  pool.query("SELECT * FROM semester_view", addResultsToResponse(res, next));
+const updateActive = query({
+  assert: (req) => {
+    if (!req.body.active) throw "continue";
+  },
+  sql: "REPLACE INTO active_semester SET id = 1, semester_id = ?",
+  using: (req, res) =>
+    req.method === CrudAction.Create
+      ? res.locals.results.insertId
+      : req.params.id,
+});
 
-const updateActive: EC = (req, res, next) => {
-  if (req.body.active) {
-    const id =
-      req.method === CrudAction.Create
-        ? res.locals.results.insertId
-        : req.params.id;
-    pool.query(
-      "REPLACE INTO active_semester SET id = 1, semester_id = ?",
-      [id],
-      addResultsToResponse(res, next, { key: "ignore", one: true })
-    );
-  } else {
-    next();
-  }
-};
+const createOne = query({
+  sql: "INSERT INTO semester SET ?",
+  using: (req) => ({
+    title: req.body.title,
+    start: req.body.start,
+    end: req.body.end,
+  }),
+});
 
-const createOne: EC = (req, res, next) =>
-  pool.query(
-    "INSERT INTO semester SET ?",
-    [{ title: req.body.title, start: req.body.start, end: req.body.end }],
-    addResultsToResponse(res, next)
-  );
+const updateOne = query({
+  sql: "UPDATE semester SET ? WHERE id = ?",
+  using: (req) => [
+    { title: req.body.title, start: req.body.start, end: req.body.end },
+    req.params.id,
+  ],
+});
 
-const updateOne: EC = (req, res, next): void => {
-  pool.query(
-    "UPDATE semester SET ? WHERE id = ?",
-    [
-      { title: req.body.title, start: req.body.start, end: req.body.end },
-      req.params.id,
-    ],
-    addResultsToResponse(res, next, { one: true })
-  );
-};
+const updateAll = [
+  query({
+    sql: "SELECT * FROM semester_view",
+  }),
+  respond({ data: (_, res) => res.locals.results, kind: "UPDATE_ALL" }),
+];
 
 export default {
   ...controllers("semester", "id"),
-  createOne: [createOne, updateActive],
+  createOne: [createOne, updateActive, updateAll],
   getCurrent,
   getMany,
-  updateOne: [updateOne, updateActive],
+  updateOne: [updateOne, updateActive, updateAll],
 };
