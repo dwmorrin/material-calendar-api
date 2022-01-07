89eeabd3218fc88047e75c07e611c093a6e7fd30
David Morrin <dm187@nyu.edu>
2022-01-07 09:48:54 -0500

    feat: socket broadcast repeats all args

--- src/sockets.ts
+++ src/sockets.ts
@@ -7,8 +7,13 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  socket.on("broadcast", (message: string) => {
-    socket.broadcast.emit("broadcast", message);
+  /**
+   * Broadcast to all users
+   * Currently no inspection of the message and data. (Just a repeater.)
+   * Could have the server do some work here if needed.
+   */
+  socket.on("broadcast", (...args: unknown[]) => {
+    socket.broadcast.emit("broadcast", ...args);
   });
 });
 
