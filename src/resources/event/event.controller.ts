e824825d9bfa1135e9e8aed23731b314a029d3c4
David Morrin <dm187@nyu.edu>
2022-01-11 16:13:00 -0500

    feat: adds validation to event unlock

--- src/resources/event/event.controller.ts
+++ src/resources/event/event.controller.ts
@@ -1,4 +1,5 @@
 import { crud, query, respond } from "../../utils/crud";
+import { EC } from "../../utils/types";
 
 const createMany = [
   query({
@@ -67,15 +68,33 @@ const lockOne = [
   }),
 ];
 
-// TODO: validation
+const unlockValidation: EC = (_, res, next) => {
+  if (!res.locals.lockUserId) return res.status(400).json({ error: "no lock" });
+  const { id, isAdmin } = res.locals.user;
+  if (res.locals.lockUserId !== id && !isAdmin)
+    return res.status(400).json({ error: "not owner" });
+  next();
+};
+
 const unlockOne = [
+  query({
+    sql: "SELECT lock_user_id FROM event WHERE id = ?",
+    using: (req) => Number(req.params.id),
+    then: (results, _, res) =>
+      (res.locals.lockUserId = results[0].lock_user_id),
+  }),
+  unlockValidation,
   query({
     sql: "UPDATE event SET lock_user_id = NULL, locked_time = NULL WHERE id = ?",
     using: (req) => Number(req.params.id),
   }),
+  query({
+    sql: "SELECT * FROM event_view WHERE id = ?",
+    using: (req) => Number(req.params.id),
+  }),
   respond({
     status: 201,
-    data: () => ({}),
+    data: (_, res) => ({ event: res.locals.results[0] }),
   }),
 ];
 
