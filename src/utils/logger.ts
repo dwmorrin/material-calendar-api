dbb2f847376fad463ab91de2e4f19cab78125cab
David Morrin <dm187@nyu.edu>
2022-01-14 13:22:35 -0500

    feat: adds debugging aids

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -12,12 +12,57 @@
 
 import { EC } from "./types";
 
+import { request } from "http";
+
+// basic http request example
+export const fetchTest: (url: string) => EC = (url) => (_, res) => {
+  const { port, hostname } = new URL(url);
+  request(
+    {
+      hostname,
+      port,
+      method: "GET",
+    },
+    (response) => {
+      res
+        .status(response.statusCode || 500)
+        .json({ data: response.statusMessage });
+    }
+  )
+    .on("error", (err) => {
+      res.status(500).json({ error: err.message });
+    })
+    .end();
+};
+
 type LoggerCB = (...args: Parameters<EC>) => string;
+
+const stringifyBody: LoggerCB = (req) => JSON.stringify(req.body);
+
+const stringifyLocals: LoggerCB = (_, res) => JSON.stringify(res.locals);
 
 const logger: (cb: LoggerCB) => EC = (cb) => (req, res, next) => {
   // eslint-disable-next-line no-console
   console.log(cb(req, res, next));
   next();
 };
+
+const responder: (cb: LoggerCB) => EC = (cb) => (req, res, next) => {
+  res.status(200).json({ data: cb(req, res, next) });
+};
+
+const makeLogAndRespondStack = (logCB: LoggerCB, dataCB: LoggerCB): EC[] => [
+  logger(logCB),
+  responder(dataCB),
+];
+
+// debugging aid
+export const checkResLocals = makeLogAndRespondStack(
+  stringifyLocals,
+  stringifyLocals
+);
+
+// debugging aid
+export const checkBody = makeLogAndRespondStack(stringifyBody, stringifyBody);
 
 export default logger;
